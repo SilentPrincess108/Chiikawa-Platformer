52bafd9c08a9b501c0b8ea3e393880a8b1b3632c
Rosan <rosan.syal@gmail.com>
2026-01-28 00:20:57 -0500

    Created and implemented new sprite animations

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export_range(0, 1) var deceleration = 0.3
 @export_range(0, 1) var acceleration = 0.3
 @export_range(0, 1) var decelerate_on_jump_release = 0.5
+@onready var animated_sprite: AnimatedSprite2D = $Sprite2D
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
@@ -22,16 +23,29 @@ func get_grav(velocity: Vector2): #change gravity when jumping and falling
 	return FALL_GRAVITY
 
 func get_input():
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	
+	#handle animation
+	if is_on_floor() and direction:
+		animated_sprite.play("walk")
+		if direction >= 1:
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
+	elif not is_on_floor():
+		animated_sprite.play("jump")
+	else:
+		animated_sprite.play("idle")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	if Input.is_action_just_released("Jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
+	
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
 	else:
