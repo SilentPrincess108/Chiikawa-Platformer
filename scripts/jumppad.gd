9aea8f2f7ff18a9eef4911de23f48b82a3444e7f
Rosan <rosan.syal@gmail.com>
2026-01-28 20:17:24 -0500

    added flan sprite for jump pad!!

--- scripts/jumppad.gd
+++ scripts/jumppad.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
-@onready var animation: AnimationPlayer = $Sprite2D/AnimationPlayer
-
+@onready var sprite: AnimatedSprite2D = $Sprite2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.get("JUMP_VELOCITY"):
 		body.velocity.y = body.JUMP_PAD_HEIGHT
-		animation.play("bounce")
+		sprite.play("bounce")
 		
