2942726dbc6b78160004c54862d78793f3fc68b9
Rosan <rosan.syal@gmail.com>
2026-01-30 23:49:08 -0500

    working on more platforms and level design

--- scripts/static_platform.gd
+++ scripts/static_platform.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $StaticBody2D/Sprite2D
 @export var texture: Texture2D
+@export var disable = false
+@onready var collision: CollisionShape2D = $StaticBody2D/CollisionShape2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sprite.texture = texture
+	collision.disabled = disable
+	
