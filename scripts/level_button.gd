8b87dc2b894b926724a1973bd275a438127fbbf4
Rosan <rosan.syal@gmail.com>
2026-03-21 10:54:02 -0400

    added level 2 sprite

--- scripts/level_button.gd
+++ scripts/level_button.gd
@@ -2,12 +2,17 @@ extends TextureButton
 
 @export var level_id: String = ""
 
-
 func _ready() -> void:
 	update()
+	self.scale = Vector2(4, 4)
+	setTexture()
 		
 func update():
 	if Globals.levels.get(level_id):
 		disabled = false
 	else:
 		disabled = true
+
+func setTexture():
+	self.texture_normal = load("res://assets/sprites/" + level_id + "_button.png")
+	self.texture_hover = load("res://assets/sprites/" + level_id + "_button_hover.png")
