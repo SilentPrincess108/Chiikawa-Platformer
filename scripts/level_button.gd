6dd5f2c17ef0e913ef1fb336163ea4eb717bf0a3
Rosan <rosan.syal@gmail.com>
2026-03-21 17:05:22 -0400

    finished level select pop ups and started working on level 2 art

--- scripts/level_button.gd
+++ scripts/level_button.gd
@@ -1,14 +1,27 @@
 extends TextureButton
 
 @export var level_id: String = ""
-@onready var popup: Popup = $Popup
+@export var level_theme: String = ""
+@onready var popup: PopupPanel = $PopupPanel
+@onready var level: Label = $PopupPanel/VBoxContainer/Level
+@onready var food: Label = $PopupPanel/VBoxContainer/Food
+@onready var state: Label = $PopupPanel/VBoxContainer/State
 
 
 func _ready() -> void:
 	update()
 	self.scale = Vector2(4, 4)
 	setTexture()
-		
+	setLabels()
+
+func setLabels():
+	level.text = level_id.replace("_", " ").capitalize()
+	food.text = "- " + level_theme.capitalize()
+	if Globals.levels[level_id]:
+		state.text = "- Unlocked"
+	else:
+		state.text = "- Locked"
+	
 func update():
 	if Globals.levels.get(level_id):
 		disabled = false
